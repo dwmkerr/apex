4debbe48c7cedd1f69f430cb89cbab4eb98df5e2
dwmkerr_cp <SND\dwmkerr_cp>
2011-08-21 10:02:57 +0000

    Freezable Collection for event bindings

--- Apex/WPF/Apex/MVVM/EventBindingCollection.cs
+++ Apex/WPF/Apex/MVVM/EventBindingCollection.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Windows.Markup;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Apex.MVVM
 {
-  public class EventBindingCollection : FrameworkElementCollection<EventBinding>
+
+  public class EventBindingCollection : FreezableCollection<EventBinding>// FrameworkElementCollection<EventBinding>
     {
+    public EventBindingCollection()
+    {
+      //Loaded += new RoutedEventHandler(EventBindingCollection_Loaded);
+    }
+    /*
+    void EventBindingCollection_Loaded(object sender, RoutedEventArgs e)
+    {
+      foreach (var child in this)
+      {
+        AddLogicalChild(child);
+        child.Bind(Parent);
+      }
+    }
+    public override void OnApplyTemplate()
+    {
+      base.OnApplyTemplate();
+      foreach (var child in this)
+      {
+        AddLogicalChild(child);
+        child.Bind(Parent);
+      }
+    }*/
     }
 }
