e2ce4008cf7a4abb9a5321d7c03b5b61cbec29f9
dwmkerr_cp <SND\dwmkerr_cp>
2011-10-28 19:35:16 +0000

    Brilliant, Event Bindings are now working in Silverlight!

--- Apex/Silverlight/Samples/CommandingSample/MainPage.xaml.cs
+++ Apex/Silverlight/Samples/CommandingSample/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using CommandingExample;
 
 namespace CommandingSample
 {
@@ -17,6 +18,25 @@ namespace CommandingSample
         public MainPage()
         {
             InitializeComponent();
+
+            ViewModel.EventsCommand.Executing += new Apex.MVVM.CancelCommandEventHandler(EventsCommand_Executing);
+            ViewModel.EventsCommand.Executed += new Apex.MVVM.CommandEventHandler(EventsCommand_Executed);
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)DataContext; }
+        }
+
+        void EventsCommand_Executed(object sender, Apex.MVVM.CommandEventArgs args)
+        {
+            ViewModel.Messages.Add("The command has finished - this is the View speaking!");
+        }
+
+        void EventsCommand_Executing(object sender, Apex.MVVM.CancelCommandEventArgs args)
+        {
+            if (MessageBox.Show("Continue with the command?", "Continue?", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
+                args.Cancel = true;
         }
     }
 }
