3832dc535c20971e6acfbe2e8f66ade8e74e4f3d
Dave Kerr <dwmkerr@gmail.com>
2011-11-22 16:00:17 +0000

    Added the DispatcherHelper class useful when Unit Testing AsynchronousCommands.

---
+++ Apex/WPF/Apex/Helpers/DispatcherHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+
+namespace Apex.Helpers
+{
+  /// <summary>
+  /// Helper class for dispatcher operations.
+  /// </summary>
+  public static class DispatcherHelper
+  {
+    /// <summary>
+    /// Allow the current dispatcher to do all events.
+    /// </summary>
+    public static void DoEvents()
+    {
+      DispatcherFrame frame = new DispatcherFrame();
+      Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, new DispatcherOperationCallback(ExitFrame), frame);
+      Dispatcher.PushFrame(frame);
+    }
+
+    /// <summary>
+    /// Used internally to exit the frame.
+    /// </summary>
+    /// <param name="frame">The frame.</param>
+    /// <returns></returns>
+    private static object ExitFrame(object frame)
+    {
+      ((DispatcherFrame)frame).Continue = false;
+      return null;
+    }
+  }
+}
