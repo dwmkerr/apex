f748e9ceece2a7ddfa07b5ebd1431227d943baeb
dwmkerr_cp <SND\dwmkerr_cp>
2012-02-02 21:31:39 +0000

    Apex.WinForms basis added

--- Apex/WPF/Apex/Controls/EnumerationComboBox.cs
+++ Apex/WPF/Apex/Controls/EnumerationComboBox.cs
@@ -28,15 +28,15 @@ namespace Apex.Controls
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="System.Windows.Controls.SelectionChangedEventArgs"/> instance containing the event data.</param>
-        void EnumerationComboBoxTemp_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        void EnumerationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-          //  Get the new item.
-          if (e.AddedItems.Count == 0 || e.AddedItems[0] is NameValue == false)
-            return;
+            //  Get the new item.
+            if (e.AddedItems.Count == 0 || e.AddedItems[0] is NameValue == false)
+                return;
 
-          //  Keep the selected enumeration up to date.
-          NameValue nameValue = e.AddedItems[0] as NameValue;
-          SelectedEnumeration = nameValue.Value;
+            //  Keep the selected enumeration up to date.
+            NameValue nameValue = e.AddedItems[0] as NameValue;
+            SelectedEnumeration = nameValue.Value;
         }
 
         /// <summary>
@@ -52,23 +52,23 @@ namespace Apex.Controls
             var enumType = SelectedEnumeration.GetType();
 
             //  Get the enum values. Use the helper rather than Enum.GetValues
-          //  as it works in Silverlight too.
+            //  as it works in Silverlight too.
             var enumValues = Apex.Helpers.EnumHelper.GetValues(enumType);
 
             //  Create some enum value/descriptions.
-            Enumerations = new List<NameValue>();
-          
+            enumerations = new List<NameValue>();
+
             //  Go through each one.
             foreach (var enumValue in enumValues)
             {
                 //  Add the enumeration item.
-              Enumerations.Add(new NameValue(((Enum)enumValue).GetDescription(), enumValue));
+                enumerations.Add(new NameValue(((Enum)enumValue).GetDescription(), enumValue));
             }
 
             //  Set the items source.
-            ItemsSource = Enumerations;
+            ItemsSource = enumerations;
 
-          //  Initialise the control.
+            //  Initialise the control.
             Initialise();
         }
 
@@ -77,76 +77,112 @@ namespace Apex.Controls
         /// </summary>
         private void Initialise()
         {
-          //  Set the display member path and selected value path.
-          DisplayMemberPath = "Name";
-          SelectedValuePath = "Value";
+            //  Set the display member path and selected value path.
+            DisplayMemberPath = "Name";
+            SelectedValuePath = "Value";
 
-          //  If we have enumerations and a selected enumeration, set the selected item.
-          if (Enumerations != null && SelectedEnumeration != null)
-          {
-            var selectedEnum = from enumeration in Enumerations where enumeration.Value.ToString() == SelectedEnumeration.ToString() select enumeration;
-            this.SelectedItem = selectedEnum.FirstOrDefault();
-          }
+            //  If we have enumerations and a selected enumeration, set the selected item.
+            if (enumerations != null && SelectedEnumeration != null)
+            {
+                var selectedEnum = from enumeration in enumerations where enumeration.Value.ToString() == SelectedEnumeration.ToString() select enumeration;
+                this.SelectedItem = selectedEnum.FirstOrDefault();
+            }
 
-          //  Wait for selection changed events.
-          SelectionChanged += new SelectionChangedEventHandler(EnumerationComboBoxTemp_SelectionChanged);
+            //  Wait for selection changed events.
+            SelectionChanged += new SelectionChangedEventHandler(EnumerationComboBox_SelectionChanged);
         }
 
         /// <summary>
-        /// The SelectedEnumerationProperty.
+        /// The SelectedEnumerationProperty dependency property.
         /// </summary>
         public static readonly DependencyProperty SelectedEnumerationProperty =
           DependencyProperty.Register("SelectedEnumeration", typeof(object), typeof(EnumerationComboBox),
 #if !SILVERLIGHT
-          new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnSelectedEnumerationChanged)));
+ new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnSelectedEnumerationChanged)));
 #else
         new PropertyMetadata(null, new PropertyChangedCallback(OnSelectedEnumerationChanged)));
 #endif
 
+        /// <summary>
+        /// Gets or sets the selected enumeration.
+        /// </summary>
+        /// <value>
+        /// The selected enumeration.
+        /// </value>
         public object SelectedEnumeration
         {
-          get { return (object)GetValue(SelectedEnumerationProperty); }
-          set { SetValue(SelectedEnumerationProperty, value); }
+            get { return (object)GetValue(SelectedEnumerationProperty); }
+            set { SetValue(SelectedEnumerationProperty, value); }
         }
 
+        /// <summary>
+        /// Called when the selected enumeration is changed.
+        /// </summary>
+        /// <param name="o">The o.</param>
+        /// <param name="args">The <see cref="System.Windows.DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private static void OnSelectedEnumerationChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
         {
-          EnumerationComboBox me = o as EnumerationComboBox;
+            //  Get the combo box.
+            EnumerationComboBox me = o as EnumerationComboBox;
 
-          //  Populate the items source.
-          me.PopulateItemsSource();
+            //  Populate the items source.
+            me.PopulateItemsSource();
         }
 
-        public List<NameValue> Enumerations
-        {
-          get;
-          set;
-        }
+        /// <summary>
+        /// Gets or sets the enumerations.
+        /// </summary>
+        /// <value>
+        /// The enumerations.
+        /// </value>
+        private List<NameValue> enumerations;
     }
 
-    public class NameValue
+    /// <summary>
+    /// A name-value pair.
+    /// </summary>
+    internal class NameValue
     {
-      public NameValue()
-      {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameValue"/> class.
+        /// </summary>
+        public NameValue()
+        {
+        }
 
-      }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameValue"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="value">The value.</param>
+        public NameValue(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
 
-      public NameValue(string name, object value)
-      {
-        Name = name;
-        Value = value;
-      }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get;
+            set;
+        }
 
-      public string Name
-      {
-        get;
-        set;
-      }
-
-      public object Value
-      {
-        get;
-        set;
-      }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public object Value
+        {
+            get;
+            set;
+        }
     }
 }
