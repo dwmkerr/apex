07ef4f3a26f33a6e6e8c004818ec088816b40e26
dwmkerr_cp <SND\dwmkerr_cp>
2012-02-15 16:26:22 +0000

    Updating to 1.5 with .NET 3.5 support

---
+++ Apex/WPF/Apex/Converters/StringToUpperCaseConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Apex.Converters
+{
+  public class StringToUpperCaseConverter : IValueConverter
+  {
+    #region IValueConverter Members
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      //  Cast the data.
+      if (value is string == false)
+        return null;
+
+      string str = value as string;
+
+      return str.ToUpper();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
