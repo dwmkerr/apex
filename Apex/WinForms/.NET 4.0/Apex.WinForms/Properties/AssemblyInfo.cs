b0be378801021341b98672931b93b5184d204dd0
Dave Kerr <dwmkerr@gmail.com>
2012-02-15 16:42:34 +0000

    Apex 1.5 for WinForms completed

--- Apex/WinForms/.NET 4.0/Apex.WinForms/Properties/AssemblyInfo.cs
+++ Apex/WinForms/.NET 4.0/Apex.WinForms/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Apex.WinForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Apex for WinForms .NET 4.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Apex.WinForms")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Dave Kerr")]
+[assembly: AssemblyProduct("Apex")]
+[assembly: AssemblyCopyright("Copyright © Dave Kerr 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
