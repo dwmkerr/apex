226307ecb5f44f2b72652ed8bbbab271b95bfe1f
Dave Kerr <dwmkerr@gmail.com>
2012-04-12 15:03:33 +0000

    First cut of the Apex View class, the Apex Broker and the Item Templates

---
+++ Apex/WPF/.NET 4.0/Apex/MVVM/ApexBroker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apex.MVVM
+{
+
+    /// <summary>
+    /// The Apex Broker can be used to get views for viewmodels.
+    /// </summary>
+    public class ApexBroker
+    {
+        /// <summary>
+        /// Registers the view for view model.
+        /// </summary>
+        /// <param name="viewModelType">Type of the view model.</param>
+        /// <param name="viewType">Type of the view.</param>
+        public void RegisterViewForViewModel(Type viewModelType, Type viewType)
+        {
+            //  Register the mapping.
+            mapViewModelTypeToViewType[viewModelType] = viewType;
+        }
+
+        /// <summary>
+        /// A map of view model types to view types.
+        /// </summary>
+        private Dictionary<Type, Type> mapViewModelTypeToViewType
+            = new Dictionary<Type, Type>();
+    }
+}
