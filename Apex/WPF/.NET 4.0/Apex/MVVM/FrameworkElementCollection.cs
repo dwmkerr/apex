8c03ce5110c230231472c147f4eb13990222b56d
Dave Kerr <dwmkerr@gmail.com>
2012-04-13 13:08:18 +0000

    Code Rearrangement - Extensions, Samples, Core and Legacy are the new root level folders. Extensions: For all VS extensions Samples: For all Core samples Core: Apex WPF/WP7/SL and WinForms Legacy: As above, but for older framework versions.

---
+++ Apex/WPF/.NET 4.0/Apex/MVVM/FrameworkElementCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Apex.MVVM
+{
+  [ContentProperty("Children")]
+  public class FrameworkElementCollection<T> : FrameworkElement where T : FrameworkElement
+  {
+    public FrameworkElementCollection()
+    {
+      Children = new List<T>();
+      this.Loaded += new RoutedEventHandler(OnLoaded);
+    }
+
+    void OnLoaded(object sender, RoutedEventArgs e)
+    {
+      foreach (FrameworkElement fe in Children)
+        this.AddLogicalChild(fe);
+    }
+
+    public List<T> Children { get; set; }
+  }
+}
