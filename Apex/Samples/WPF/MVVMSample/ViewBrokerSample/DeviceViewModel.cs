58435793a04c909cc501d1225ecf948f22e9e399
Dave Kerr <dwmkerr@gmail.com>
2012-04-16 15:50:00 +0000

    More work on snippets and also the next gen mvvm samples

---
+++ Apex/Samples/WPF/MVVMSample/ViewBrokerSample/DeviceViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+
+namespace MVVMSample.ViewBrokerSample
+{
+    /// <summary>
+    /// The DeviceViewModel ViewModel class.
+    /// </summary>
+    public class DeviceViewModel : ViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceViewModel"/> class.
+        /// </summary>
+        public DeviceViewModel()
+        {
+        }
+
+        
+        private NotifyingProperty NameProperty =
+          new NotifyingProperty("Name", typeof(string), default(string));
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        
+        private NotifyingProperty DescriptionProperty =
+          new NotifyingProperty("Description", typeof(string), default(string));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        
+        private NotifyingProperty ReleaseDateProperty =
+          new NotifyingProperty("ReleaseDate", typeof(DateTime), default(DateTime));
+
+        public DateTime ReleaseDate
+        {
+            get { return (DateTime)GetValue(ReleaseDateProperty); }
+            set { SetValue(ReleaseDateProperty, value); }
+        }
+                
+                
+                
+
+        /// <summary>
+        /// The components.
+        /// </summary>
+        private ObservableCollection<ComponentViewModel> components = 
+            new ObservableCollection<ComponentViewModel>();
+
+        /// <summary>
+        /// Gets or sets the components.
+        /// </summary>
+        /// <value>
+        /// The components.
+        /// </value>
+        public ObservableCollection<ComponentViewModel> Components
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Apex/Samples/WPF/MVVMSample/ViewBrokerSample/ViewBrokerSampleView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Apex.MVVM;
+
+namespace MVVMSample.ViewBrokerSample
+{
+    /// <summary>
+    /// Interaction logic for ViewBrokerSampleView.xaml
+    /// </summary>
+    public partial class ViewBrokerSampleView : ViewBrokerSampleViewBase
+    {
+        public ViewBrokerSampleView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    /// <summary>
+    /// Base class for the ViewBrokerSampleView View.
+    /// Until such time as XAML supports generics, we must define a base
+    /// class explicitly for the view so that we can provide it in the XAML
+    /// markup.
+    /// </summary>
+    public partial class ViewBrokerSampleViewBase : View<ViewBrokerSampleViewModel>
+    {
+    }
+}
