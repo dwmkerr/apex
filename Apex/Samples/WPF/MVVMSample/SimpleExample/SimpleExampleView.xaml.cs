06fad034f53cab401af234d050c6f171595e4e6e
dwmkerr_cp <SND\dwmkerr_cp>
2012-04-16 15:50:00 +0000

    More work on snippets and also the next gen mvvm samples

---
+++ Apex/Samples/WPF/MVVMSample/SimpleExample/SimpleExampleView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Apex.MVVM;
+
+namespace MVVMSample.SimpleExample
+{
+    /// <summary>
+    /// Interaction logic for SimpleExampleView.xaml
+    /// </summary>
+    public partial class SimpleExampleView : SimpleExampleViewBase
+    {
+        public SimpleExampleView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    /// <summary>
+    /// Base class for the SimpleExampleView View.
+    /// Until such time as XAML supports generics, we must define a base
+    /// class explicitly for the view so that we can provide it in the XAML
+    /// markup.
+    /// </summary>
+    public partial class SimpleExampleViewBase : View<SimpleExampleViewModel>
+    {
+    }
+}
---
+++ Apex/Samples/WPF/MVVMSample/ViewBrokerSample/ViewBrokerSampleViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+
+namespace MVVMSample.ViewBrokerSample
+{
+    /// <summary>
+    /// The ViewBrokerSampleViewModel ViewModel class.
+    /// </summary>
+    public class ViewBrokerSampleViewModel : ViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewBrokerSampleViewModel"/> class.
+        /// </summary>
+        public ViewBrokerSampleViewModel()
+        {
+            //  Create some devices.
+            var ipad = new DeviceViewModel() { Name = "Apple iPad", Description = "The Apple iPad." };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private ObservableCollection<DeviceViewModel> devices =
+            new ObservableCollection<DeviceViewModel>();
+
+        /// <summary>
+        /// Gets the devices.
+        /// </summary>
+        public ObservableCollection<DeviceViewModel> Devices
+        {
+            get { return devices; }
+        }
+
+    }
+}
