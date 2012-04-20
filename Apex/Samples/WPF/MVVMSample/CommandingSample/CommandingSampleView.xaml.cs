076dd82190dc144c0bff3eb7de5004cecdf803f2
dwmkerr_cp <SND\dwmkerr_cp>
2012-04-20 13:28:06 +0000

    MVVM Sample updated with commanding sample

---
+++ Apex/Samples/WPF/MVVMSample/CommandingSample/CommandingSampleView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Apex.MVVM;
+
+namespace MVVMSample.CommandingSample
+{
+    /// <summary>
+    /// Interaction logic for CommandingSampleView.xaml
+    /// </summary>
+    public partial class CommandingSampleView : CommandingSampleViewBase
+    {
+        public CommandingSampleView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    /// <summary>
+    /// Base class for the SimpleExampleView View.
+    /// Until such time as XAML supports generics, we must define a base
+    /// class explicitly for the view so that we can provide it in the XAML
+    /// markup.
+    /// </summary>
+    public partial class CommandingSampleViewBase : View<CommandingSampleViewModel>
+    {
+    }
+}
