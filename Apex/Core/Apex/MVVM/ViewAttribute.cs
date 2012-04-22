361eb56eae8d1da5f6bface4d88a0f0531c789ec
dwmkerr_cp <SND\dwmkerr_cp>
2012-04-22 15:58:24 +0000

    Wee bit more on the broker

--- Apex/Core/Apex/MVVM/ViewAttribute.cs
+++ Apex/Core/Apex/MVVM/ViewAttribute.cs
@@ -32,12 +32,33 @@ namespace Apex.MVVM
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="ViewAttribute"/> class.
+        /// </summary>
+        /// <param name="viewModelType">Type of the associated ViewModel.</param>
+        /// <param name="hint">The hint.</param>
+        public ViewAttribute(Type viewModelType, string hint)
+        {
+            //  Store the view model type.
+            ViewModelType = viewModelType;
+            Hint = hint;
+        }
+
+        /// <summary>
         /// Gets the type of the view model.
         /// </summary>
         /// <value>
         /// The type of the view model.
         /// </value>
         public Type ViewModelType
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the hint.
+        /// </summary>
+        public string Hint
         {
             get;
             private set;
