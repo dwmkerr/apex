5378b70061cbecf3bbd859bac901469672b8ff70
dwmkerr_cp <SND\dwmkerr_cp>
2012-04-23 14:29:37 +0000

    Singleton template started

--- Apex/Extensions/SingletonItemTemplate/Properties/AssemblyInfo.cs
+++ Apex/Extensions/SingletonItemTemplate/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("676a80f9-8ed9-4c7a-85da-862acda556b6")]
+[assembly: Guid("b0844797-476a-434b-86c9-5474a886a81f")]
 
 // Version information for an assembly consists of the following four values:
 //
