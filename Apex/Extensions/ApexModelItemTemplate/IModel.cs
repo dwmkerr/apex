253f11f66b47143ecefed3051b74e093c5ebc26d
dwmkerr_cp <SND\dwmkerr_cp>
2012-04-25 21:30:31 +0000

    More work on the Apex Model Template

---
+++ Apex/Extensions/ApexModelItemTemplate/IModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// The <see cref="$safeitemrootname$"/> Model Interface class.
+    /// </summary>
+    public interface $safeitemrootname$
+    {
+        //  Add functions to the model here.
+    }
+}
---
+++ Apex/Extensions/ApexModelItemTemplate/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Apex.MVVM;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// The <see cref="$safeitemrootname$"/> Model Singleton class.
+    /// This class implements the <see cref="I$safeitemrootname$"/> Model interface.
+    /// You can retrieve the model with:
+    /// <code>I$safeitemrootname$ model = ApexBroker.GetModel<I$safeitemrootname$>()</code>
+    /// </summary>
+    public sealed class $safeitemrootname$ : I$safeitemrootname$
+    {
+        /// <summary>
+        /// The Singleton instace. Declared 'static readonly' to enforce
+        /// a single instance only and lazy initialisation.
+        /// </summary>
+        private static readonly $safeitemrootname$ instance = new $safeitemrootname$();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="$safeitemrootname$"/> class.
+        /// Declared private to enforce a single instance only.
+        /// </summary>
+        private $safeitemrootname$() 
+        {
+            //  Register the model with the global broker.
+            ApexBroker.RegisterModel<I$safeitemrootname$>(this);
+        }
+
+        /// <summary>
+        /// Gets the $safeitemrootname$ Singleton Instance.
+        /// </summary>
+        public static $safeitemrootname$ Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
