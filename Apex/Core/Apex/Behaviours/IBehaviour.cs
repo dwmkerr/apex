e30eff10778ba2a18068cc7fa37db7d984e14107
Dave Kerr <dwmkerr@gmail.com>
2012-05-01 13:37:07 +0000

    Behaviours checkin

---
+++ Apex/Core/Apex/Behaviours/IBehaviour.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Apex.Behaviours
+{
+
+    /// <summary>
+    /// Implement this interface in a Behaviour class to allow it to be attached or detached.
+    /// </summary>
+    public interface IBehaviour
+    {
+        /// <summary>
+        /// Attaches the behaviour to the specified framework element.
+        /// </summary>
+        /// <param name="frameworkElement">The framework element.</param>
+        void Attach(FrameworkElement frameworkElement);
+
+        /// <summary>
+        /// Detaches the behaviour to the the specified framework element.
+        /// </summary>
+        /// <param name="frameworkElement">The framework element.</param>
+        void Detach(FrameworkElement frameworkElement);
+    }
+}
