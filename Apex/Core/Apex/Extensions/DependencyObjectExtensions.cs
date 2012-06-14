f0972f027b18d77547574a664e6f71903cce5de4
dwmkerr_cp <SND\dwmkerr_cp>
2012-06-14 10:05:40 +0000

    Interim: Re-arranging extensions folder structure ready to add the Silverlight Extensions

--- Apex/Core/Apex/Extensions/DependencyObjectExtensions.cs
+++ Apex/Core/Apex/Extensions/DependencyObjectExtensions.cs
@@ -13,252 +13,222 @@ using System.Windows.Markup.Primitives;
 
 namespace Apex.Extensions
 {
-  /// <summary>
-  /// A set of useful extensions for the DependencyObject class.
-  /// </summary>
-  public static class DependencyObjectExtensions
-  {
     /// <summary>
-    /// Gets the first parent found of a specific type.
+    /// A set of useful extensions for the DependencyObject class.
     /// </summary>
-    /// <typeparam name="T">The type of parent to find.</typeparam>
-    /// <param name="child">The child.</param>
-    /// <returns>The first parent found of type 'T' or null if no parent of type 'T' is found.</returns>
-    public static T GetParent<T>(this DependencyObject child) where T : DependencyObject
+    public static class DependencyObjectExtensions
     {
-      //  Get the visual parent.
-      DependencyObject dependencyObject = VisualTreeHelper.GetParent(child);
-
-      //  If we've got the parent, return it if it is the correct type - otherwise
-      //  continue up the tree.
-      if (dependencyObject != null)
-        return dependencyObject is T ? dependencyObject as T : GetParent<T>(dependencyObject);
-      else
-        return null;
-    }
-
-    public static DependencyObject GetTopLevelParent(this DependencyObject child)
-    {
-        DependencyObject tmp = child;
-        DependencyObject parent = null;
-        while ((tmp = VisualTreeHelper.GetParent(tmp)) != null)
+        /// <summary>
+        /// Gets the first parent found of a specific type.
+        /// </summary>
+        /// <typeparam name="T">The type of parent to find.</typeparam>
+        /// <param name="child">The child.</param>
+        /// <returns>The first parent found of type 'T' or null if no parent of type 'T' is found.</returns>
+        public static T GetParent<T>(this DependencyObject child) where T : DependencyObject
         {
-            parent = tmp;
+            //  Get the visual parent.
+            DependencyObject dependencyObject = VisualTreeHelper.GetParent(child);
+
+            //  If we've got the parent, return it if it is the correct type - otherwise
+            //  continue up the tree.
+            if (dependencyObject != null)
+                return dependencyObject is T ? dependencyObject as T : GetParent<T>(dependencyObject);
+            else
+                return null;
         }
-        return parent;
-    }
 
-    public static List<T> GetChildObjects<T>(this DependencyObject obj, string name)
-    {
-      var retVal = new List<T>();
-      for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
-      {
-        object c = VisualTreeHelper.GetChild(obj, i);
-        if ((c.GetType().FullName == typeof(T).FullName || c.GetType().IsSubclassOf(typeof(T))) && (String.IsNullOrEmpty(name) || ((FrameworkElement)c).Name == name))
-        { 
-          retVal.Add((T)c);
-        }
-        var gc = ((DependencyObject)c).GetChildObjects<T>(name);
-        if (gc != null)
-          retVal.AddRange(gc);
-      }
-
-      return retVal;
-    }
-
-    /// <summary>
-    /// Gets all children of a specified type, through the visual tree.
-    /// This function recurses.
-    /// </summary>
-    /// <typeparam name="T">The type of child to get.</typeparam>
-    /// <param name="me">The dependency object to get children of.</param>
-    /// <returns>All children of type T of the dependency object.</returns>
-    public static IEnumerable<T> GetVisualChildren<T>(this DependencyObject me) where T : DependencyObject
-    {
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(me); i++)
+        public static DependencyObject GetTopLevelParent(this DependencyObject child)
         {
-            DependencyObject child = VisualTreeHelper.GetChild(me, i);
-            if (child != null && child is T)
-                yield return (T)child;
-
-            foreach (T childOfChild in child.GetVisualChildren<T>())
-                yield return childOfChild;
-        }
-    }
-
-#if !SILVERLIGHT4 && !SILVERLIGHT3
-
-    /// <summary>
-    /// Gets all children of a specified type, through the logical tree.
-    /// This function recurses.
-    /// </summary>
-    /// <typeparam name="T">The type of child to get.</typeparam>
-    /// <param name="me">The dependency object to get children of.</param>
-    /// <returns>All children of type T of the dependency object.</returns>
-    public static IEnumerable<T> GetLogicalChildren<T>(this DependencyObject me) where T : DependencyObject
-    {
-        foreach(var child in LogicalTreeHelper.GetChildren(me))
-        {
-            //  If the child is not a dependency object, we can't use it.
-            var childDependencyObject = child as DependencyObject;
-            if (childDependencyObject == null)
-                continue;
-
-            if (childDependencyObject != null && childDependencyObject is T)
-                yield return (T)childDependencyObject;
-
-            foreach (T childOfChild in childDependencyObject.GetLogicalChildren<T>())
-                yield return childOfChild;
-        }
-    }
-
-#endif
-
-    public static T GetChildObject<T>(this DependencyObject obj, string name) where T : DependencyObject
-    {
-      for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
-      {
-        object c = VisualTreeHelper.GetChild(obj, i);
-        if (c.GetType().FullName == typeof(T).FullName && (String.IsNullOrEmpty(name) || ((FrameworkElement)c).Name == name))
-        {
-          return (T)c;
-        }
-        object gc = ((DependencyObject)c).GetChildObject<T>(name);
-        if (gc != null)
-          return (T)gc;
-      }
-
-      return null;
-    }
-
-#if !SILVERLIGHT
-
-    public static IEnumerable<T> GetChildren<T>(this DependencyObject parent) where T : DependencyObject
-    {
-        return parent.GetChildren<T>(false);
-    }
-
-    public static IEnumerable<T> GetChildren<T>(this DependencyObject parent, bool recursive) where T : DependencyObject
-    {
-      if (parent == null) yield break;
-
-      if (parent is ContentElement || parent is FrameworkElement)
-      {
-        //use the logical tree for content / framework elements
-        foreach (object obj in LogicalTreeHelper.GetChildren(parent))
-        {
-          var depObj = obj as DependencyObject;
-          if (depObj != null) yield return (T)obj;
-          if (recursive)
-          {
-              var more = depObj.GetChildren<T>(true);
-              foreach (var i in more)
-                  yield return i;
-          }
-        }
-      }
-      else
-      {
-        //use the visual tree per default
-        int count = VisualTreeHelper.GetChildrenCount(parent);
-        for (int i = 0; i < count; i++)
-        {
-            T child = (T)VisualTreeHelper.GetChild(parent, i);
-            yield return child;
-            if (recursive)
+            DependencyObject tmp = child;
+            DependencyObject parent = null;
+            while ((tmp = VisualTreeHelper.GetParent(tmp)) != null)
             {
-                var more = child.GetChildren<T>(true);
-                foreach (var ch in more)
-                    yield return ch;
+                parent = tmp;
+            }
+            return parent;
+        }
+
+        /// <summary>
+        /// Gets all children of a specified type, through the visual tree.
+        /// This function recurses.
+        /// </summary>
+        /// <typeparam name="T">The type of child to get.</typeparam>
+        /// <param name="me">The dependency object to get children of.</param>
+        /// <returns>All children of type T of the dependency object.</returns>
+        public static IEnumerable<T> GetVisualChildren<T>(this DependencyObject me) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(me); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(me, i);
+                if (child != null && child is T)
+                    yield return (T) child;
+
+                foreach (T childOfChild in child.GetVisualChildren<T>())
+                    yield return childOfChild;
             }
         }
-      }
-    }
-
-#endif
-
-    public static T FindChild<T>(this DependencyObject me, string childName) where T : DependencyObject
-    {
-      // Confirm parent and childName are valid. 
-      if (me == null) return null;
-
-      T foundChild = null;
-
-      int childrenCount = VisualTreeHelper.GetChildrenCount(me);
-      for (int i = 0; i < childrenCount; i++)
-      {
-        var child = VisualTreeHelper.GetChild(me, i);
-        // If the child is not of the request child type child
-        T childType = child as T;
-        if (childType == null)
-        {
-          // recursively drill down the tree
-          foundChild = FindChild<T>(child, childName);
-
-          // If the child is found, break so we do not overwrite the found child. 
-          if (foundChild != null) break;
-        }
-        else if (!string.IsNullOrEmpty(childName))
-        {
-          var frameworkElement = child as FrameworkElement;
-          // If the child's name is set for search
-          if (frameworkElement != null && frameworkElement.Name == childName)
-          {
-            // if the child's name is of the request name
-            foundChild = (T)child;
-            break;
-          }
-        }
-        else
-        {
-          // child element found.
-          foundChild = (T)child;
-          break;
-        }
-      }
-
-      return foundChild;
-    }
 
 #if !SILVERLIGHT
 
-    public static List<Binding> GetBindingObjects(this DependencyObject me)
-    {
-      List<Binding> bindings = new List<Binding>();
-      List<DependencyProperty> dpList = GetDependencyProperties(me);
-      
-      foreach (DependencyProperty dp in dpList)
-      {
-        Binding b = BindingOperations.GetBinding(me, dp);
-        if (b != null)
+        /// <summary>
+        /// Gets all children of a specified type, through the logical tree.
+        /// This function recurses.
+        /// </summary>
+        /// <typeparam name="T">The type of child to get.</typeparam>
+        /// <param name="me">The dependency object to get children of.</param>
+        /// <returns>All children of type T of the dependency object.</returns>
+        public static IEnumerable<T> GetLogicalChildren<T>(this DependencyObject me) where T : DependencyObject
         {
-          bindings.Add(b);
+            foreach (var child in LogicalTreeHelper.GetChildren(me))
+            {
+                //  If the child is not a dependency object, we can't use it.
+                var childDependencyObject = child as DependencyObject;
+                if (childDependencyObject == null)
+                    continue;
+
+                if (childDependencyObject != null && childDependencyObject is T)
+                    yield return (T) childDependencyObject;
+
+                foreach (T childOfChild in childDependencyObject.GetLogicalChildren<T>())
+                    yield return childOfChild;
+            }
         }
-      }
 
-      return bindings;
-    }
-
-    public static List<DependencyProperty> GetDependencyProperties(this DependencyObject me)
-    {
-
-      List<DependencyProperty> attached = new List<DependencyProperty>();
-
-      foreach (PropertyDescriptor pd in TypeDescriptor.GetProperties(me,
-          new Attribute[] { new PropertyFilterAttribute(PropertyFilterOptions.All) }))
-      {
-        DependencyPropertyDescriptor dpd =
-            DependencyPropertyDescriptor.FromProperty(pd);
-
-        if (dpd != null)
-        {
-          attached.Add(dpd.DependencyProperty);
-        }
-      }
-
-      return attached;
-    }
 #endif
-  }
+
+        /// <summary>
+        /// Finds a child element of a specified type with a specified name.
+        /// </summary>
+        /// <typeparam name="T">The type of child element to find.</typeparam>
+        /// <param name="me">The dependency object.</param>
+        /// <param name="childName">Name of the child.</param>
+        /// <returns>The first child of type T with the specified name, or null of no
+        /// children are found.</returns>
+        public static T FindChild<T>(this DependencyObject me, string childName) where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (me == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(me);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(me, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T) child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T) child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+#if !SILVERLIGHT
+
+        public static IEnumerable<T> GetChildren<T>(this DependencyObject parent) where T : DependencyObject
+        {
+            return parent.GetChildren<T>(false);
+        }
+
+        public static IEnumerable<T> GetChildren<T>(this DependencyObject parent, bool recursive)
+            where T : DependencyObject
+        {
+            if (parent == null) yield break;
+
+            if (parent is ContentElement || parent is FrameworkElement)
+            {
+                //use the logical tree for content / framework elements
+                foreach (object obj in LogicalTreeHelper.GetChildren(parent))
+                {
+                    var depObj = obj as DependencyObject;
+                    if (depObj != null) yield return (T) obj;
+                    if (recursive)
+                    {
+                        var more = depObj.GetChildren<T>(true);
+                        foreach (var i in more)
+                            yield return i;
+                    }
+                }
+            }
+            else
+            {
+                //use the visual tree per default
+                int count = VisualTreeHelper.GetChildrenCount(parent);
+                for (int i = 0; i < count; i++)
+                {
+                    T child = (T) VisualTreeHelper.GetChild(parent, i);
+                    yield return child;
+                    if (recursive)
+                    {
+                        var more = child.GetChildren<T>(true);
+                        foreach (var ch in more)
+                            yield return ch;
+                    }
+                }
+            }
+        }
+
+#endif
+
+#if !SILVERLIGHT
+
+        /// <summary>
+        /// Gets the binding objects.
+        /// </summary>
+        /// <param name="me">The dependency object.</param>
+        /// <returns>All bindings for the dependency object.</returns>
+        public static IEnumerable<Binding> GetBindingObjects(this DependencyObject me)
+        {
+            //  Get the dependency properties.
+            var dpList = GetDependencyProperties(me);
+
+            //  Select all of the non-null bindings.
+            return dpList.Select(dp => BindingOperations.GetBinding(me, dp)).Where(b => b != null);
+        }
+
+        /// <summary>
+        /// Gets the dependency properties of a dependency object.
+        /// </summary>
+        /// <param name="me">The dependency object.</param>
+        /// <returns>The dependency properties of a dependency object.</returns>
+        public static IEnumerable<DependencyProperty> GetDependencyProperties(this DependencyObject me)
+        {
+            //  Get appropriate properties.
+            var properties = TypeDescriptor.GetProperties(me,
+                                                          new Attribute[]
+                                                              {new PropertyFilterAttribute(PropertyFilterOptions.All)});
+
+            //  Return all non null dependency properties.
+            return from PropertyDescriptor pd in properties 
+                   select DependencyPropertyDescriptor.FromProperty(pd) into dpd 
+                   where dpd != null 
+                   select dpd.DependencyProperty;
+        }
+#endif
+    }
 
 }
--- Apex/Samples/WPF/MVVMSample/ViewBrokerActivationSample/Page1ViewModel.cs
+++ Apex/Samples/WPF/MVVMSample/ViewBrokerActivationSample/Page1ViewModel.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Apex.Controls;
 using Apex.MVVM;
 
 namespace MVVMSample.ViewBrokerActivationSample
 {
     [ViewModel]
-    public class Page1ViewModel : ViewModel
+    public class Page1ViewModel : ViewModel, ISelectableItem
     {
         public string Title
         {
             get { return "Page 1"; }
+        }
+
+        public void OnSelected()
+        {
+        }
+
+        public void OnDeselected()
+        {
+        }
+
+        private readonly NotifyingProperty isSelectedProperty = new NotifyingProperty("IsSelected", typeof(bool), default(bool));
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(isSelectedProperty); }
+            set { SetValue(isSelectedProperty, value); }
         }
     }
 }
