3c5090a60b4a6eba6ff0a4e60f56f935ebc81f2a
dwmkerr_cp <SND\dwmkerr_cp>
2012-06-18 17:13:31 +0000

    Debugging code for the SL seletableitemscontrol bug

---
+++ Apex/Sandbox/Sandbox/WpfSelectionBug/OnionView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Apex.MVVM;
+
+namespace WpfSelectionBug
+{
+    /// <summary>
+    /// Interaction logic for OnionView.xaml
+    /// </summary>
+    [View(typeof(OnionPageViewModel))]
+    public partial class OnionView : UserControl, IView
+    {
+        public OnionView()
+        {
+            InitializeComponent();
+        }
+
+        public void OnActivated()
+        {
+        }
+
+        public void OnDeactivated()
+        {
+        }
+    }
+}
---
+++ Apex/Sandbox/Sandbox/WpfSelectionBug/Pages.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+
+namespace WpfSelectionBug
+{
+    [ViewModel]
+    public class FruitPageGroupViewModel : PageViewModel
+    {
+        public FruitPageGroupViewModel()
+        {
+            Title = "Fruit";
+        }
+    }
+
+    [ViewModel]
+    public class ApplePageViewModel : PageViewModel
+    {
+        public ApplePageViewModel()
+        {
+            Title = "Apple";
+        }
+    }
+
+    [ViewModel]
+    public class PearPageViewModel : PageViewModel
+    {
+        public PearPageViewModel()
+        {
+            Title = "Pear";
+        }
+    }
+
+    [ViewModel]
+    public class VegPageGroupViewModel : PageViewModel
+    {
+        public VegPageGroupViewModel()
+        {
+            Title = "Veg";
+        }
+    }
+
+    [ViewModel]
+    public class OnionPageViewModel : PageViewModel
+    {
+        public OnionPageViewModel()
+        {
+            Title = "Onion";
+        }
+    }
+
+    [ViewModel]
+    public class CarrotPageViewModel : PageViewModel
+    {
+        public CarrotPageViewModel()
+        {
+            Title = "Carrot";
+        }
+    }
+}
