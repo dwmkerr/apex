a28c7fe011246f39edbe645af61031d0e4c395f4
dwmkerr_cp <SND\dwmkerr_cp>
2012-07-08 15:45:35 +0000

    Added the CueTextBox, a TextBox that has an optional cue. WPF only at this stage.

---
+++ Apex/Core/Apex/Controls/CueTextBox.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Apex.Controls
+{
+    /// <summary>
+    /// The CueDisplayMode enumeration allows the behaviour of a CueTextBox's cue's
+    /// visibilty to be controlled.
+    /// </summary>
+    public enum CueDisplayMode
+    {
+        /// <summary>
+        /// Hide the cue when the CueTextBox has text.
+        /// </summary>
+        HideWhenHasText,
+
+        /// <summary>
+        /// Hide the cue when the CueTextBox has text or is focussed.
+        /// </summary>
+        HideWhenHasTextOrFocus
+    }
+
+    /// <summary>
+    /// The CueTextBox is a standard TextBox control that can show a 'cue' or 'hint'. The cue is a faint
+    /// label in the text box area that instructs the user on what they should enter in the text box.
+    /// The cue can be set via the CueText property.
+    /// </summary>
+    [TemplatePart(Name = "PART_CueLabel", Type = typeof(Label))]
+    public class CueTextBox : TextBox
+    {
+        /// <summary>
+        /// Initializes the <see cref="CueTextBox"/> class.
+        /// </summary>
+        static CueTextBox()
+        {
+            //  Override the default style. 
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CueTextBox), new FrameworkPropertyMetadata(typeof(CueTextBox)));
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.UIElement.LostFocus"/> event (using the provided arguments).
+        /// </summary>
+        /// <param name="e">Provides data about the event.</param>
+        protected override void  OnLostFocus(RoutedEventArgs e)
+        {
+            //  Call the base.
+ 	         base.OnLostFocus(e);
+
+            //  Update the cue visibilty.
+            UpdateCueVisiblity();
+        }
+
+        /// <summary>
+        /// Invoked whenever an unhandled <see cref="E:System.Windows.UIElement.GotFocus"/> event reaches this element in its route.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.Windows.RoutedEventArgs"/> that contains the event data.</param>
+        protected override void  OnGotFocus(RoutedEventArgs e)
+        {
+            //  Call the base.
+ 	         base.OnGotFocus(e);
+
+            //  Update the cue visibilty.
+            UpdateCueVisiblity();
+        }
+
+        /// <summary>
+        /// Is called when a control template is applied.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            //  Call the base.
+            base.OnApplyTemplate();
+
+            try
+            {
+                //  Get the cue labe.
+                cueLabel = (Label)GetTemplateChild("PART_CueLabel");
+            }
+            catch
+            {
+                throw new Exception("Unable to access the internal elements of the CueTextBox.");
+            }
+
+        }
+
+        /// <summary>
+        /// Is called when content in this editing control changes.
+        /// </summary>
+        /// <param name="e">The arguments that are associated with the <see cref="E:System.Windows.Controls.Primitives.TextBoxBase.TextChanged"/> event.</param>
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+
+            //  Update the cue visibilty.
+            UpdateCueVisiblity();
+        }
+
+        /// <summary>
+        /// Updates the cue visiblity.
+        /// </summary>
+        private void UpdateCueVisiblity()
+        {
+            //  We set the cue visibilty based on the cue display mode.
+            switch(CueDisplayMode)
+            {
+                case CueDisplayMode.HideWhenHasTextOrFocus:
+                    cueLabel.Visibility = IsFocused || !string.IsNullOrEmpty(Text) ? Visibility.Hidden : Visibility.Visible;
+                    break;
+                case CueDisplayMode.HideWhenHasText:
+                    cueLabel.Visibility = string.IsNullOrEmpty(Text) ? Visibility.Visible : Visibility.Hidden;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// The cue label.
+        /// </summary>
+        private Label cueLabel;
+        
+        /// <summary>
+        /// The DependencyProperty for the CueText property.
+        /// </summary>
+        public static readonly DependencyProperty CueTextProperty =
+          DependencyProperty.Register("CueText", typeof(string), typeof(CueTextBox),
+          new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// Gets or sets CueText.
+        /// </summary>
+        /// <value>The value of CueText.</value>
+        public string CueText
+        {
+            get { return (string)GetValue(CueTextProperty); }
+            set { SetValue(CueTextProperty, value); }
+        }
+        
+        /// <summary>
+        /// The DependencyProperty for the CueDisplayMode property.
+        /// </summary>
+        public static readonly DependencyProperty CueDisplayModeProperty =
+          DependencyProperty.Register("CueDisplayMode", typeof(CueDisplayMode), typeof(CueTextBox),
+          new PropertyMetadata(CueDisplayMode.HideWhenHasTextOrFocus, new PropertyChangedCallback(OnCueDisplayModeChanged)));
+
+        /// <summary>
+        /// Gets or sets CueDisplayMode.
+        /// </summary>
+        /// <value>The value of CueDisplayMode.</value>
+        public CueDisplayMode CueDisplayMode
+        {
+            get { return (CueDisplayMode)GetValue(CueDisplayModeProperty); }
+            set { SetValue(CueDisplayModeProperty, value); }
+        }
+
+        /// <summary>
+        /// Called when cue display mode changed.
+        /// </summary>
+        /// <param name="o">The o.</param>
+        /// <param name="args">The <see cref="System.Windows.DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
+        private static void OnCueDisplayModeChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
+        {
+            var cueTextBox = (CueTextBox)o;
+            cueTextBox.UpdateCueVisiblity();
+        }
+    }
+}
--- Apex/Core/Apex/Converters/StringNullOrEmptyToVisibilityConverter.cs
+++ Apex/Core/Apex/Converters/StringNullOrEmptyToVisibilityConverter.cs
@@ -7,26 +7,64 @@ using System.Windows;
 
 namespace Apex.Converters
 {
-  public class StringNullOrEmptyToVisibilityConverter : IValueConverter
-  {
-    #region IValueConverter Members
-
-    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    /// <summary>
+    /// The StringNullOrEmptyToVisibility converter takes a string as its input.
+    /// If the string is null or empty the converter will return Visibility.Collapsed, otherwise
+    /// it will return Visibility.Visible. You can invert this behaviour by setting the 
+    /// converter parameter to 'Invert'.
+    /// </summary>
+    public class StringNullOrEmptyToVisibilityConverter : IValueConverter
     {
-      //  Cast the data.
-      if (value is string == false)
-        return Visibility.Collapsed;
+        #region IValueConverter Members
 
-      string str = value as string;
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //  If we don't have a string, we must fail.
+            if (value is string == false)
+                return Visibility.Collapsed;
 
-      return string.IsNullOrEmpty(str) ? Visibility.Collapsed : Visibility.Visible;
+            //  Get the string.
+            string str = (string)value;
+
+            //  Determine whether we are inverting.
+            bool invert = (parameter is string && ((string)parameter).IndexOf("Invert", StringComparison.OrdinalIgnoreCase) != -1);
+
+            //  Are we collapsing?
+            bool collapse = string.IsNullOrEmpty(str);
+
+            //  Apply inversion.
+            if (invert)
+                collapse = !collapse;
+
+            //  Return the correct visibilty.
+            return collapse ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
-
-    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-    {
-      throw new NotImplementedException();
-    }
-
-    #endregion
-  }
 }
