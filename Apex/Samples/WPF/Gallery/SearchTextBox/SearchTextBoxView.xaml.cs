47e8eb21bc114aefcea0558fa67ab90b67914e03
Dave Kerr <dwmkerr@gmail.com>
2012-07-08 16:06:09 +0000

    Added the first cut of the SearchTextBox, based on the CueTextBox

---
+++ Apex/Samples/WPF/Gallery/SearchTextBox/SearchTextBoxView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gallery.SearchTextBox
+{
+    /// <summary>
+    /// Interaction logic for SearchTextBoxView.xaml
+    /// </summary>
+    public partial class SearchTextBoxView : UserControl
+    {
+        public SearchTextBoxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
