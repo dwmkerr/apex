612d73aaa174cd12bd85db5f4b78dd894f34330e
dwmkerr_cp <SND\dwmkerr_cp>
2012-07-14 17:57:49 +0000

    More work on the gallery

---
+++ Apex/Samples/WPF/Gallery/PivotControl/PivotControlView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Gallery.PivotControl
+{
+    /// <summary>
+    /// Interaction logic for PivotControlView.xaml
+    /// </summary>
+    public partial class PivotControlView : UserControl
+    {
+        public PivotControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
