f49fc9bab595725e9be0e06301ff4889f8dfdc3e
Dave Kerr <dwmkerr@gmail.com>
2012-12-06 22:32:25 +0000

    wnforms path text box first cut

---
+++ Apex/Core/Apex.WinForms/Controls/PathTextBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Apex.WinForms.Controls
+{
+    /// <summary>
+    /// The Path Text Box is a text box that has a browse button.
+    /// </summary>
+    public class PathTextBox : TextBox
+    {
+        /// <summary>
+        /// Raises the <see cref="M:System.Windows.Forms.Control.CreateControl"/> method.
+        /// </summary>
+        protected override void OnCreateControl()
+        {
+            //  Call the base.
+            base.OnCreateControl();
+
+            //  Create the button.
+            CreateBrowseButton();
+        }
+
+        /// <summary>
+        /// Creates the browse button.
+        /// </summary>
+        private void CreateBrowseButton()
+        {
+            //  Create the browse button.
+            browseButton = new Button();
+            browseButton.Parent = this;
+            browseButton.Text = "...";
+            browseButton.Size = new Size(ButtonWidth, ClientRectangle.Height - (ButtonPadding * 2));
+            browseButton.Location = new Point(ClientRectangle.Right - (ButtonWidth + ButtonPadding), ButtonPadding);
+            browseButton.CreateControl();
+        }
+
+        private const int ButtonWidth = 20;
+        private const int ButtonPadding = 0;//1;
+
+        private Button browseButton;
+    }
+}
--- Apex/Samples/WinForms/ControlsSample/FormControlsSample.Designer.cs
+++ Apex/Samples/WinForms/ControlsSample/FormControlsSample.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ControlsSample
+﻿using Apex.WinForms.Controls;
+
+namespace ControlsSample
 {
     partial class FormControlsSample
     {
@@ -30,25 +32,29 @@
         {
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageColorMap = new System.Windows.Forms.TabPage();
-            this.colorMap = new Apex.Controls.ColorMap();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPageExpandingPanel = new System.Windows.Forms.TabPage();
+            this.tabPagePathTextBox = new System.Windows.Forms.TabPage();
+            this.colorMap = new Apex.Controls.ColorMap();
             this.expandingPanelContainer1 = new Apex.Controls.ExpandingPanelContainer();
-            this.expandingPanel1 = new Apex.Controls.ExpandingPanel();
             this.expandingPanel2 = new Apex.Controls.ExpandingPanel();
+            this.expandingPanel1 = new Apex.Controls.ExpandingPanel();
+            this.textBox1 = new PathTextBox();
             this.tabControl.SuspendLayout();
             this.tabPageColorMap.SuspendLayout();
             this.tabPageExpandingPanel.SuspendLayout();
+            this.tabPagePathTextBox.SuspendLayout();
             this.expandingPanelContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl
             // 
-            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl.Controls.Add(this.tabPageColorMap);
             this.tabControl.Controls.Add(this.tabPageExpandingPanel);
+            this.tabControl.Controls.Add(this.tabPagePathTextBox);
             this.tabControl.Location = new System.Drawing.Point(12, 12);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
@@ -66,18 +72,6 @@
             this.tabPageColorMap.TabIndex = 0;
             this.tabPageColorMap.Text = "Color Map";
             this.tabPageColorMap.UseVisualStyleBackColor = true;
-            // 
-            // colorMap
-            // 
-            this.colorMap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.colorMap.BackColor = System.Drawing.Color.Red;
-            this.colorMap.CurrentColor = System.Drawing.Color.Red;
-            this.colorMap.Location = new System.Drawing.Point(18, 35);
-            this.colorMap.Name = "colorMap";
-            this.colorMap.ShowColorBorder = true;
-            this.colorMap.Size = new System.Drawing.Size(228, 94);
-            this.colorMap.TabIndex = 1;
             // 
             // label1
             // 
@@ -99,11 +93,34 @@
             this.tabPageExpandingPanel.Text = "Expanding Panel";
             this.tabPageExpandingPanel.UseVisualStyleBackColor = true;
             // 
+            // tabPagePathTextBox
+            // 
+            this.tabPagePathTextBox.Controls.Add(this.textBox1);
+            this.tabPagePathTextBox.Location = new System.Drawing.Point(4, 22);
+            this.tabPagePathTextBox.Name = "tabPagePathTextBox";
+            this.tabPagePathTextBox.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePathTextBox.Size = new System.Drawing.Size(252, 212);
+            this.tabPagePathTextBox.TabIndex = 2;
+            this.tabPagePathTextBox.Text = "Path Text Box";
+            this.tabPagePathTextBox.UseVisualStyleBackColor = true;
+            // 
+            // colorMap
+            // 
+            this.colorMap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.colorMap.BackColor = System.Drawing.Color.Red;
+            this.colorMap.CurrentColor = System.Drawing.Color.Red;
+            this.colorMap.Location = new System.Drawing.Point(18, 35);
+            this.colorMap.Name = "colorMap";
+            this.colorMap.ShowColorBorder = true;
+            this.colorMap.Size = new System.Drawing.Size(228, 94);
+            this.colorMap.TabIndex = 1;
+            // 
             // expandingPanelContainer1
             // 
-            this.expandingPanelContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.expandingPanelContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.expandingPanelContainer1.AutoScroll = true;
             this.expandingPanelContainer1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.expandingPanelContainer1.Controls.Add(this.expandingPanel2);
@@ -113,27 +130,34 @@
             this.expandingPanelContainer1.Size = new System.Drawing.Size(240, 200);
             this.expandingPanelContainer1.TabIndex = 1;
             // 
-            // expandingPanel1
-            // 
-            this.expandingPanel1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.expandingPanel1.Location = new System.Drawing.Point(4, 4);
-            this.expandingPanel1.Name = "expandingPanel1";
-            this.expandingPanel1.PanelContainer = this.expandingPanelContainer1;
-            this.expandingPanel1.PanelName = "Panel 1";
-            this.expandingPanel1.Size = new System.Drawing.Size(215, 100);
-            this.expandingPanel1.SizeWidthToContainer = true;
-            this.expandingPanel1.TabIndex = 1;
-            // 
             // expandingPanel2
             // 
             this.expandingPanel2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.expandingPanel2.Location = new System.Drawing.Point(4, 108);
+            this.expandingPanel2.Location = new System.Drawing.Point(4, 4);
             this.expandingPanel2.Name = "expandingPanel2";
             this.expandingPanel2.PanelContainer = this.expandingPanelContainer1;
             this.expandingPanel2.PanelName = "Panel 2";
-            this.expandingPanel2.Size = new System.Drawing.Size(215, 100);
+            this.expandingPanel2.Size = new System.Drawing.Size(192, 100);
             this.expandingPanel2.SizeWidthToContainer = true;
             this.expandingPanel2.TabIndex = 2;
+            // 
+            // expandingPanel1
+            // 
+            this.expandingPanel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.expandingPanel1.Location = new System.Drawing.Point(4, 108);
+            this.expandingPanel1.Name = "expandingPanel1";
+            this.expandingPanel1.PanelContainer = this.expandingPanelContainer1;
+            this.expandingPanel1.PanelName = "Panel 1";
+            this.expandingPanel1.Size = new System.Drawing.Size(192, 100);
+            this.expandingPanel1.SizeWidthToContainer = true;
+            this.expandingPanel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // FormControlsSample
             // 
@@ -147,6 +171,8 @@
             this.tabPageColorMap.ResumeLayout(false);
             this.tabPageColorMap.PerformLayout();
             this.tabPageExpandingPanel.ResumeLayout(false);
+            this.tabPagePathTextBox.ResumeLayout(false);
+            this.tabPagePathTextBox.PerformLayout();
             this.expandingPanelContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -162,6 +188,8 @@
         private Apex.Controls.ExpandingPanelContainer expandingPanelContainer1;
         private Apex.Controls.ExpandingPanel expandingPanel2;
         private Apex.Controls.ExpandingPanel expandingPanel1;
+        private System.Windows.Forms.TabPage tabPagePathTextBox;
+        private PathTextBox textBox1;
     }
 }
 
