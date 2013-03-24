8dc8880ffe0567aecf28d135bd8deef287d68fb2
dwmkerr_cp <SND\dwmkerr_cp>
2013-03-24 21:35:31 +0000

    Added a sample for the Folder Text Box

---
+++ Apex/Gallery/Gallery.WPF/Controls/FolderTextBox/FolderTextBoxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gallery.Controls.FolderTextBox
+{
+    public class FolderTextBoxViewModel : GalleryItemViewModel
+    {
+        public FolderTextBoxViewModel()
+        {
+            Title = "FolderTextBox";
+        }
+    }
+}
