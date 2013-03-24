e8d5ff24a06dddfcfbf5b30404bfaf17aab348ff
Dave Kerr <dwmkerr@gmail.com>
2013-03-24 14:05:34 +0000

    More work on the gallery.

---
+++ Apex/Gallery/Gallery.WPF/Controls/ControlsView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Apex.MVVM;
+
+namespace Gallery.Controls
+{
+    /// <summary>
+    /// Interaction logic for ControlsView.xaml
+    /// </summary>
+    [View(typeof(ControlsViewModel))]
+    public partial class ControlsView : UserControl
+    {
+        public ControlsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Apex/Gallery/Gallery.WPF/Controls/ControlsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+using Gallery.Controls.ApexGrid;
+using Gallery.Controls.CrossButton;
+using Gallery.Controls.EnumComboBox;
+using Gallery.Controls.MultiBorder;
+using Gallery.Controls.PaddedGrid;
+using Gallery.Controls.TabbedDocumentInterface;
+using Gallery.CueTextBox;
+using Gallery.PathTextBox;
+using Gallery.PivotControl;
+using Gallery.SearchTextBox;
+
+namespace Gallery.Controls
+{
+    [ViewModel]
+    public class ControlsViewModel : GalleryItemViewModel
+    {
+        public ControlsViewModel()
+        {
+            Title = "Controls";
+
+            GalleryItems.Add(new ApexGridViewModel());
+            GalleryItems.Add(new CrossButtonViewModel());
+            GalleryItems.Add(new CueTextBoxViewModel());
+            GalleryItems.Add(new EnumComboBoxViewModel());
+            GalleryItems.Add(new MultiBorderViewModel());
+            GalleryItems.Add(new PaddedGridViewModel());
+            GalleryItems.Add(new PathTextBoxViewModel());
+            GalleryItems.Add(new PivotControlViewModel());
+            GalleryItems.Add(new SearchTextBoxViewModel());
+            GalleryItems.Add(new TabbedDocumentInterfaceViewModel());
+        }
+    }
+}
